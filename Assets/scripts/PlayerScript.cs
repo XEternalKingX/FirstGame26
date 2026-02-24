d6f334d6c33c5c82fa9273be7373ff34536b1841
XEternalKingX <zoeyac02@gmail.com>
2026-02-24 16:18:41 -0600

    Walking Animation
    Drew out a walking spritesheet and worked it into the game.

--- Assets/scripts/PlayerScript.cs
+++ Assets/scripts/PlayerScript.cs
@@ -1,35 +1,41 @@
 using UnityEngine;
 
-public class player_movement : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
-
     [SerializeField] private float speed;
-    private Rigidbody2D body;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D body;
+    private Animator anim;
+
     void Start()
     {
         GameSettings.Load();
         speed = GameSettings.PlayerSpeed;
 
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
+
         body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
 
-        //Flip player when facing left/right.
-        if (horizontalInput > 0.01f)
-            transform.localScale = new Vector3(-0.15f, 0.15f, 0.15f);
+        // Walking animation
+        if (anim != null)
+        {
+            bool isWalking = Mathf.Abs(horizontalInput) > 0.01f;
+            anim.SetBool("isWalking", isWalking);
+        }
 
-        else if (horizontalInput < -0.01f)
+        // Flip player
+        if (horizontalInput > 0.01f)
             transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-0.15f, 0.15f, 0.15f);
 
         if (Input.GetKey(KeyCode.Space))
             body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
-
     }
 }
